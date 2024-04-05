b07eb635ade88935a3361671378f0dae9a77bce5
Matheus <rdesouzamatheus@gmail.com>
2024-04-05 19:56:10 -0300

    ContextBase configurado na classe program_Intalado SqlServer na Api

--- Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0ae6f8822d49f69573e26bcf6b39a20662b3895")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+044793c5a386bfbbfc1ac70b7b56456e313c4e52")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
