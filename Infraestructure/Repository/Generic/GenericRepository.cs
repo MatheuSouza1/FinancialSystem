173f54e39349ce5b0799ac83498c295ee40b439c
Matheus <rdesouzamatheus@gmail.com>
2024-04-11 19:15:56 -0300

    Métodos GenericRepository implementados

--- Infraestructure/Repository/Generic/GenericRepository.cs
+++ Infraestructure/Repository/Generic/GenericRepository.cs
@@ -1,37 +1,98 @@
 ﻿using Domain.Interfaces.Generics.Repository;
+using Infraestructure.Configurations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32.SafeHandles;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Infraestructure.Repository.Generic
 {
-    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    public class GenericRepository<T> : IGenericRepository<T>, IDisposable where T : class
     {
-        public Task Add(T entity)
+        private readonly DbContextOptions<ContextBase> _Options;
+        public GenericRepository() 
         {
-            throw new NotImplementedException();
+            _Options = new DbContextOptions<ContextBase>();
         }
 
-        public Task Delete(T entity)
+        public async Task Add(T entity)
         {
-            throw new NotImplementedException();
+            using (var data = new ContextBase(_Options))
+            {
+                await data.Set<T>().AddAsync(entity);
+                await data.SaveChangesAsync();
+            }
         }
 
-        public Task<List<T>> GetAll()
+        public async Task Delete(T entity)
         {
-            throw new NotImplementedException();
+            using (var data = new ContextBase(_Options))
+            {
+                data.Set<T>().Remove(entity);
+                await data.SaveChangesAsync();
+            }
         }
 
-        public Task<T> GetById(int id)
+        public async Task<List<T>> GetAll()
         {
-            throw new NotImplementedException();
+            using (var data = new ContextBase(_Options))
+            {
+                return await data.Set<T>().ToListAsync();
+            }
         }
 
-        public Task Update(T entity)
+        public async Task<T> GetById(int id)
         {
-            throw new NotImplementedException();
+            using (var data = new ContextBase(_Options))
+            {
+                return await data.Set<T>().FindAsync(id);
+            }
         }
+
+        public async Task Update(T entity)
+        {
+            using (var data = new ContextBase(_Options))
+            {
+                data.Set<T>().Update(entity);
+                await data.SaveChangesAsync();
+            }
+        }
+
+        //implementação da interface IDisposable abaixo:
+        #region Disposed https://docs.microsoft.com/pt-br/dotnet/standard/garbage-collection/implementing-dispose
+        // Flag: Has Dispose already been called?
+        bool disposed = false;
+        // Instantiate a SafeHandle instance.
+        SafeHandle handle = new SafeFileHandle(IntPtr.Zero, true);
+
+        // Public implementation of Dispose pattern callable by consumers.
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+
+        // Protected implementation of Dispose pattern.
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+
+            if (disposing)
+            {
+                handle.Dispose();
+                // Free any other managed objects here.
+                //
+            }
+
+            disposed = true;
+        }
+        #endregion
+        // fim
     }
 }
